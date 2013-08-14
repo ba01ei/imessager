bb6b2a3fdf7c33d1794ea6ed24cd90c8e46c0551
Bao Lei <bao.lei@hulu.com>
2013-08-13 23:41:58 -0700

    added test mode for gmail.rb

--- plugins/gmail.rb
+++ plugins/gmail.rb
@@ -2,17 +2,21 @@ require 'gmail'
 
 class GMail
 
-  def self.run(conf)
+  def self.run(conf, testing)
+    trace_back = testing ? 1000000 : 2
     output = []
     conf["accounts"].each do |account|
       Gmail.connect(account["username"], account["password"]) do |g|
-        mails = g.inbox.emails(:unread, :after => (Time.now - 300*conf["period"]))
+        mails = g.inbox.emails(:unread, :after => (Time.now - trace_back*60*conf["period"]))
+        puts "new emails for #{account["username"]}: #{mails.count}"
         mails.each do |m|
          bodytext = "#{m.text_part.body}"
          output << "#{m.from[0].name}: #{m.subject}\n#{bodytext[0..199]}"
+         m.read!
         end
       end
     end
+    puts output.join("\n") if testing
     output
   end
   
