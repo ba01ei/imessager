6e3b8454338b92efa034d3e001c491445e842502
Bao Lei <bao.lei@hulu.com>
2013-08-14 15:07:30 -0700

    Added empty config.rb

---
+++ config.rb
@@ -0,0 +1,16 @@
+
+TRIGGERS = {
+  "GMail" => {
+    "accounts" => [
+      {"username" => "", 
+      "password" => "",
+      },  # You can add more than one account
+    ],
+    "period" => 5  # polling period in minutes
+  }
+}
+
+RECEIVERS = [
+  "",  # iPhone phone number here
+]
+
