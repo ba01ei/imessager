897b2105d1c47a3a575c58a03db49f39db0aa836
Bao Lei <bao.lei@hulu.com>
2013-08-14 14:23:34 -0700

    in case of string error, send symbol removed version to imessage, instead of email

--- lib/imessage_sender.rb
+++ lib/imessage_sender.rb
@@ -27,11 +27,12 @@ class IMessageSender
 
   # sends an imessage
   def self.send(phone, text)
-    puts "apple script: #{self.apple_script(phone, text)}"
+    # puts "apple script: #{self.apple_script(phone, text)}"
     result = system("osascript -e '#{self.apple_script(phone, text)}'")
     unless result
-      puts "error executing applescript. sending email.."
-      `echo "#{text.gsub(/[^0-9a-z]/i, ' ')}" | mail -s "error executing applescript" baolei1234@gmail.com`
+      puts "error executing applescript. sending simplified version.."
+      simple = text.gsub(/[^0-9a-z]/i, ' ')
+      system("osascript -e '#{self.apple_script(phone, simple)}'")
     end
   end
 end
