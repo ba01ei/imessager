2edf0103a40be23ce8406788c865f5b54242782a
Bao Lei <bao.lei@hulu.com>
2013-08-13 21:32:10 -0700

    imessage sender

---
+++ lib/imessage_sender.rb
@@ -0,0 +1,23 @@
+
+class IMessageSender
+
+  # constructs the apple script  
+  def self.apple_script(phone, text)
+    script = "tell application \"Messages\" to activate\n" +
+             "tell application \"System Events\"\n" +
+             "  keystroke \"n\" using {command down}\n" +
+             "  keystroke \"#{phone}\"\n" +
+             "  keystroke return\n" +
+             "  keystroke tab\n" +
+             "  keystroke \"#{text.gsub("\"", "\\\"")}\"\n" +
+             "  keystroke return\n" +
+             "end tell"
+    script
+  end
+
+  # sends an imessage
+  def self.send(phone, text)
+    `osascript -e '#{self.apple_script(phone, text)}'`
+  end
+end
+
