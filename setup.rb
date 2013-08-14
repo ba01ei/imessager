c29a31080f3d6441b872036055585eb4e5f823a5
Bao Lei <bao.lei@hulu.com>
2013-08-14 14:39:14 -0700

    better setup.rb, remove old crons

--- setup.rb
+++ setup.rb
@@ -5,11 +5,21 @@ RUNNER = File.join(DIR, "lib", "run_tasks.rb")
 require File.join(DIR, "config.rb")
 
 if __FILE__ == $0
-  script = "crontab -l > mycron\n";
-  TRIGGERS.each do |trigger, config|
-    script += "echo \"*/#{config["period"]}\t*\t*\t*\t*\tbash -l -c 'ruby #{RUNNER} #{trigger}'\" >> mycron\n"
+  script = `crontab -l`;
+  lines = script.split("\n")
+  # puts lines.count
+  lines.each do |line|
+    if line.index("run_tasks")
+      lines.delete(line)
+    end
   end
-  script += "crontab mycron\n" +
-            "rm mycron\n"
+  TRIGGERS.each do |trigger, config|
+    lines << "*/#{config["period"]}\t*\t*\t*\t*\tbash -l -c 'ruby #{RUNNER} #{trigger}'"
+  end
+  File.open("mycron", "w") do |f|
+    f.write(lines.join("\n") + "\n")
+  end
+  script = "crontab mycron\n" +
+           "rm mycron\n"
   `#{script}`
 end
