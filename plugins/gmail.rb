89c62a68cda41c285b22b06f3863705e3f6eabb5
Bao Lei <bao.lei@hulu.com>
2013-08-21 21:41:14 -0700

    Added a keep field in gmail conf, so you can keep getting bugged by one message until you manually open the mail

--- plugins/gmail.rb
+++ plugins/gmail.rb
@@ -16,10 +16,22 @@ class GMail
         mails = g.inbox.emails(:unread, :after => (Time.now - trace_back))
         puts "new emails for #{account["username"]}: #{mails.count}" if testing
         mails.each do |m|
-         bodytext = m.text_part ? m.text_part.body.to_s : (m.body.to_s.length > 0 ? m.body.to_s : "")
-         t = Time.now.localtime
-         output << "#{m.from[0].name}: #{m.subject} === #{bodytext}".gsub("&amp;", "&").gsub(/=\?WINDOWS-\d+\?Q\?/, "")[0..512] + " (#{t.hour}:#{t.min})"
-         m.read!
+          bodytext = m.text_part ? m.text_part.body.to_s : (m.body.to_s.length > 0 ? m.body.to_s : "")
+          t = Time.now.localtime
+          output << "#{m.from[0].name}: #{m.subject} === #{bodytext}".gsub("&amp;", "&").gsub(/=\?WINDOWS-\d+\?Q\?/, "")[0..512] + " (#{t.hour}:#{t.min})"
+          keep = false
+          subject_l = m.subject.downcase
+          conf["keep"].each do |query|
+              if subject_l.index(query.downcase)
+                keep = true
+              break
+            end
+          end
+          if keep
+            m.unread!
+          else
+            m.read!
+          end
         end
       end
     end
