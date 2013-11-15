886eab69fde910af35897f243ba117cc6a39dd4a
Bao Lei <bao.lei@hulu.com>
2013-11-14 22:19:05 -0800

    be able to encode password

--- setup.rb
+++ setup.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
+require 'base64'
+require 'aescrypt'
+
 DIR = File.expand_path(File.dirname(__FILE__))
 RUNNER = File.join(DIR, "lib", "run_tasks.rb")
-require File.join(DIR, "config.rb")
+CONFIG = File.join(DIR, "config.rb")
+require CONFIG
 
-if __FILE__ == $0
+def setup_cron
   script = `crontab -l`;
   old_lines = script.split("\n")
   lines = []
@@ -25,4 +29,45 @@ if __FILE__ == $0
   script = "crontab mycron\n" +
            "rm mycron\n"
   `#{script}`
+end
+
+def setup_keys
+  reveal = ARGV[0] == "reveal"
+  key_file = File.join(DIR, "data", ".key")
+  old_private_key = File.exist?(key_file) ? File.read(key_file) : nil
+  new_private_key = Base64.encode64(rand().to_s).strip
+  File.write(key_file, new_private_key)
+  conf = File.read(CONFIG)
+  conf.scan(/(\"key\"\s*\=\>\s*\"([^\"]+)\")/).each do |pubkey|
+    line = pubkey[0]
+    key = pubkey[1]
+    if old_private_key
+      # puts "AESCrypt.decrypt(#{key}, #{old_private_key})"
+      pass = AESCrypt.decrypt(key, old_private_key)
+      new_pub_key = AESCrypt.encrypt(pass, new_private_key).strip
+      if reveal
+        conf.gsub!(line, "\"password\" => \"#{pass}\"")
+      else
+        conf.gsub!(line, "\"key\" => \"#{new_pub_key}\"")
+      end
+    else
+      # oops, missing old key
+      conf.gsub!(line, "\"password\" => \"enter your password\"")
+    end
+  end
+  if not reveal
+    conf.scan(/(\"password\"\s*\=\>\s*\"([^\"]+)\")/).each do |password|
+      line = password[0]
+      pass = password[1]
+      new_pub_key = AESCrypt.encrypt(pass, new_private_key).strip
+      conf.gsub!(line, "\"key\" => \"#{new_pub_key}\"")
+    end
+  end
+
+  File.write(CONFIG, conf)
+end
+
+if __FILE__ == $0
+  setup_cron
+  setup_keys
 end
